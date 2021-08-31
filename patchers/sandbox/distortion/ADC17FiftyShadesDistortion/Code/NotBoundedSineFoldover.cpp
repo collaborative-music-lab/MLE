56e2754292e0ae4fb6fbd20edc33b1f6fa9f8141
Ian Hattwick <ian@ianhattwick.com>
2021-08-31 19:33:16 -0400

    Revert "Revert "Removed all""
    This reverts commit 9287a10adb88bcc2c122c6c86d3d2e88097111bd.

--- patchers/sandbox/distortion/ADC17FiftyShadesDistortion/Code/NotBoundedSineFoldover.cpp
+++
@@ -1,4 +0,0 @@
-/** Not bounded sine foldover waveshaper algorithm. */
-
-for (auto i = 0; i < numSamples; i++)
-    samples[i] = samples[i] + gain * std::sin (factor * samples[i]);
--- patchers/sandbox/distortion/ADC17FiftyShadesDistortion/Code/TanhWaveshaper.cpp
+++
@@ -1,4 +0,0 @@
-/** Tanh waveshaper algorithm. */
-
-for (auto i = 0; i < numSamples; i++)
-    std::tanh (samples[i]);
